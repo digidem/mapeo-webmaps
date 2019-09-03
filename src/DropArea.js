844c8af1bdb626671d675d99e7459562ad990107
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-09-03 15:37:24 +0100

    feat: Add file drop area

---
+++ src/DropArea.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { makeStyles } from "@material-ui/styles";
+import Typography from "@material-ui/core/Typography";
+import BalanceText from "react-balance-text";
+import Button from "@material-ui/core/Button";
+import Icon from "@material-ui/icons/CloudUploadOutlined";
+
+const defaultInputProps = {
+  type: "file",
+  style: { display: "none" },
+  tabIndex: -1
+};
+
+export default function DropArea({
+  inputProps = defaultInputProps,
+  isDragging
+}) {
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      <Icon className={classes.icon} fontSize="inherit" />
+      <Typography
+        component={BalanceText}
+        className={classes.text}
+        align="center"
+        gutterBottom
+      >
+        Drag & Drop a <span className={classes.mono}>.mapeomap</span> file here
+        to create a new map
+      </Typography>
+      <Typography align="center" gutterBottom>
+        — or —
+      </Typography>
+      <input {...inputProps} id="contained-button-file" />
+      <label htmlFor="contained-button-file">
+        <Button
+          color="primary"
+          variant="outlined"
+          component="span"
+          className={classes.button}
+        >
+          Select file
+        </Button>
+      </label>
+    </div>
+  );
+}
+
+const useStyles = makeStyles({
+  container: {
+    border: "dashed 3px #cccccc",
+    borderRadius: 10,
+    padding: 10,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    minHeight: "15em",
+    color: "#666666"
+  },
+  text: {
+    maxWidth: "20em"
+  },
+  mono: {
+    fontFamily: "monospace",
+    fontSize: "1.3em"
+  },
+  icon: {
+    fontSize: 64,
+    color: "#aaaaaa"
+  }
+});
---
+++ src/DropArea.stories.js
@@ -0,0 +1,8 @@
+import React from "react";
+import DropArea from "./DropArea";
+
+export default {
+  title: "DropArea"
+};
+
+export const initial = () => <DropArea />;
