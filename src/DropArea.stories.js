eeb7d939e9aad23a8acf0098765bfd31807b7780
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-09-05 15:53:38 +0100

    chore: fix DropArea stories

--- src/DropArea.stories.js
+++ src/DropArea.stories.js
@@ -2,9 +2,10 @@ import React from "react";
 import DropArea from "./DropArea";
 
 export default {
-  title: "DropArea"
+  title: "DropArea",
+  decorators: [storyFn => <div style={{ maxWidth: 640 }}>{storyFn()}</div>]
 };
 
 export const initial = () => <DropArea />;
 
-export const dragging = () => <DropArea isDragging />;
+export const dragging = () => <DropArea isDragActive />;
