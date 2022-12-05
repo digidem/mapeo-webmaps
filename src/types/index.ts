aa1b1c69c6a6dec2b7299b94dee28608995ae405
elys <>
2022-12-05 16:24:51 +0100

    chore: add firebaseConfig type

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type firebaseConfigType = {
+  projectId: string
+  appId: string
+  storageBucket: string
+  locationId: string
+  apiKey: string
+  authDomain: string
+  messagingSenderId: string
+  measurementId: string
+}
+
+export type ChildType = React.ReactElement // Single child element
