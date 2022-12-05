ed540636f72b5a971347b497a7ad2fa3ce0a9819
elys <>
2022-12-05 18:10:30 +0100

    chore: add back in router

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,25 +1,64 @@
-import { ThemeProvider, Typography } from '@mui/material';
-import SplitLayout from '../../layouts/split';
+import { ThemeProvider } from '@mui/material';
+
 import CssBaseline from "@mui/material/CssBaseline";
+import { useAuthState } from "react-firebase-hooks/auth";
+import firebase from "firebase/app";
+
+import { Router, Link, navigate } from "@reach/router"
+
+import Login from '../../views/Login'
 
 import theme from '../../theme';
+import { IntlProvider } from 'react-intl';
+import { useEffect } from 'react';
+import HomeView from '../../views/Home';
+import { AuthorizedProps, translationsType } from './types';
+
+const translations: translationsType = {
+  es: require("../../translations/es.json")
+};
+
+
+const Authorized = ({ location }: AuthorizedProps) => {
+  const [user, initializing] = useAuthState(firebase.auth());
+  // Is the user authorized to see this page? An unauthorised user can see any
+  // path in the publicPaths map
+  const isAuthorized = user || location?.pathname.startsWith("/auth");
+  useEffect(() => {
+    console.log({ loc: location?.pathname })
+    if (isAuthorized || initializing) return;
+    // Redirect unauthorized users to the login page, but keep state of where
+    // they come from
+    navigate("/auth/login", {
+      replace: true,
+      state: { from: location?.pathname }
+    });
+  }, [isAuthorized, initializing, location]);
+
+  return (
+    <HomeView />
+  );
+};
+
+const lang = navigator.language ? navigator.language.split("-")[0] : "en";
 
 
 const App = () => (
+  // <IntlProvider locale={lang} messages={translations[lang]} defaultLocale="en">
   <>
     <CssBaseline />
     <ThemeProvider theme={theme}>
-      <SplitLayout>
-        <Typography variant="h1" component="h2">
-          Left
-        </Typography>
-        <Typography variant="h1" component="h2">
-          Right
-        </Typography>
+      <Router>
+        <Login path="/auth/login" />
+        <Authorized path="/*" />
+        {/* <Signup path="/auth/signup" /> */}
+        {/* <ResetPassword path="/auth/reset-password" /> */}
+        {/* <Authorized path="/*" /> */}
+      </Router>
 
-      </SplitLayout>
     </ThemeProvider>
   </>
+  // </IntlProvider>
 );
 
 export default App;
