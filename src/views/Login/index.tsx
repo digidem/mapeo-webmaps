c732856cfee378f8fe161f96f9c1eeb1a61a0475
elys <>
2022-12-05 18:10:30 +0100

    chore: add back in router

---
+++ src/views/Login/index.tsx
@@ -0,0 +1,19 @@
+import { Typography } from "@mui/material"
+import SplitLayout from "../../layouts/split"
+import { LoginViewTypes } from "./types"
+
+const LoginView = ({ }: LoginViewTypes) => {
+  return (
+    <SplitLayout>
+      <Typography variant="h1" component="h2">
+        Left
+      </Typography>
+      <Typography variant="h1" component="h2">
+        Right
+      </Typography>
+
+    </SplitLayout>
+  )
+}
+
+export default LoginView
