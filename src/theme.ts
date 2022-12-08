e4430b10c33a37aec4bc9f9481c208b7468708bb
elys <>
2022-12-08 17:25:31 +0100

    add typing to theme config

--- src/theme.ts
+++ src/theme.ts
@@ -1,10 +1,48 @@
-import { createTheme } from "@mui/system";
+import { createTheme } from "@mui/material";
+
+
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    primary: React.CSSProperties['color']
+    background: React.CSSProperties['color']
+    blueDark: React.CSSProperties['color']
+    warningRed: React.CSSProperties['color']
+    black: React.CSSProperties['color']
+    orange: React.CSSProperties['color']
+    white: React.CSSProperties['color']
+    limeGreen: {
+      main: React.CSSProperties['color']
+      light: React.CSSProperties['color']
+    }
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    primary: React.CSSProperties['color']
+    background: React.CSSProperties['color']
+    blueDark: React.CSSProperties['color']
+    warningRed: React.CSSProperties['color']
+    black: React.CSSProperties['color']
+    orange: React.CSSProperties['color']
+    white: React.CSSProperties['color']
+    limeGreen: {
+      main: React.CSSProperties['color']
+      light: React.CSSProperties['color']
+    }
+  }
+}
 
 const mapeoBlue = '#0066FF'
+const backgroundLight = '#F4F4FF'
 const mapeoBlack = '#333333'
 const mapeoOrange = '#EA7913'
 const darkBlue = '#000033'
-const warningRed = '#D92222'
+const warningRed = '#CB355B'
+const white = '#FFFFFF'
+const limeGreen = {
+  main: '#C4FFD5',
+  light: '#E5FFE5'
+}
 
 const theme = createTheme({
   typography: {
@@ -20,17 +58,16 @@ const theme = createTheme({
       '"Segoe UI Symbol"',
     ].join(','),
   },
+  background: backgroundLight,
+  blueDark: darkBlue,
+  primary: mapeoBlue,
+  warningRed,
+  black: mapeoBlack,
+  orange: mapeoOrange,
+  white,
+  limeGreen,
   palette: {
-    background: {
-      light: '#F4F4FF',
-      dark: darkBlue,
-    },
-    primary: mapeoBlue,
-    error: {
-      main: warningRed
-    },
-    black: mapeoBlack,
-    orange: mapeoOrange
+    primary: { main: mapeoBlue }
   }
 });
 
