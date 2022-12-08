7dc1e168a6ae8173c66665abfd302ec54832ff14
elys <>
2022-12-08 17:25:16 +0100

    style: remove semi-colons because

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import firebase from "firebase/app";
+import React from "react"
+import ReactDOM from "react-dom"
+import firebase from "firebase/app"
 
 // Add the Firebase services that you want to use
-import "firebase/auth";
-import "firebase/firestore";
-import "firebase/storage";
+import "firebase/auth"
+import "firebase/firestore"
+import "firebase/storage"
 
-import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
-import { firebaseConfigType } from "./types";
+import App from "./components/App"
+import * as serviceWorker from "./serviceWorker"
+import { firebaseConfigType } from "./types"
 
 if (process.env.NODE_ENV === "development") {
   // Use staging credentials when running locally
@@ -22,34 +22,34 @@ if (process.env.NODE_ENV === "development") {
     authDomain: "mapeo-webmaps-staging.firebaseapp.com",
     messagingSenderId: "354071501370",
     measurementId: "G-GLMDFRWPJD",
-  });
+  })
 } else {
   // Get credentials according to environment (production, staging)
   // https://firebase.google.com/docs/hosting/reserved-urls
   fetch("/__/firebase/init.json").then(async (response) => {
-    init(await response.json());
-  });
+    init(await response.json())
+  })
 }
 
 function init(firebaseConfig: firebaseConfigType) {
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig)
 
-  const db = firebase.firestore();
+  const db = firebase.firestore()
 
   if (process.env.NODE_ENV === "development") {
     // Use emulated firestore and auth when running locally
-    db.useEmulator("localhost", 8080);
-    firebase.auth().useEmulator("http://localhost:9099/");
+    db.useEmulator("localhost", 8080)
+    firebase.auth().useEmulator("http://localhost:9099/")
   }
 
   // For some reason, if we don't call this here, writes fail silently in the app
   // Enables offline persistence
-  db.enablePersistence();
+  db.enablePersistence()
 
-  ReactDOM.render(<App />, document.getElementById("root"));
+  ReactDOM.render(<App />, document.getElementById("root"))
 }
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
