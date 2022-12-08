119bc0b7959362807b6e7ca84161de5eb645ba94
elys <>
2022-12-08 17:26:18 +0100

    chore: improvements to split layout and other bits

--- src/layouts/split/types.ts
+++ src/layouts/split/types.ts
@@ -1,13 +1,12 @@
-
-type ChildType = React.ReactElement | Element
+import { ChildType } from "../../types"
 
 type GridStyleType = {
-  backgroundColor: string,
-  color: string
+  backgroundColor?: string,
+  color?: string
 }[]
 
 export type SplitLayoutTypes = {
-  children: [ChildType, ChildType] | [ChildType],
+  children: [ChildType, ChildType] | [ChildType]
   columns?: number,
   gridStyles?: GridStyleType,
   grid?: [number] | [number, number],
