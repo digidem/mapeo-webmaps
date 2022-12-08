3fe1231a91370a1172ab39d9adaed0b6cd77cb24
elys <>
2022-12-08 17:26:18 +0100

    chore: improvements to split layout and other bits

--- src/layouts/split/index.tsx
+++ src/layouts/split/index.tsx
@@ -1,48 +1,38 @@
-import { Box } from "@mui/material";
-import Grid from "@mui/material/Unstable_Grid2";
-import { SplitLayoutTypes } from "./types";
+import { Box, useTheme } from '@mui/material'
+import Grid from '@mui/material/Unstable_Grid2'
+import { SplitLayoutTypes } from './types'
 
-const defaultGridStyles = [
-  {
-    backgroundColor: "background.dark",
-    color: "white",
-  },
-  {
-    backgroundColor: "background.light",
-    color: "black",
-  },
-];
+const SplitLayout = ({ children, grid = [4, 8], gridStyles, columns = 12 }: SplitLayoutTypes) => {
+  const theme = useTheme()
 
-export const SplitLayout = ({
-  children,
-  grid = [5, 7],
-  gridStyles = defaultGridStyles,
-  columns = 12,
-}: SplitLayoutTypes) => {
+  const defaultGridStyles = [{
+    backgroundColor: theme.blueDark,
+    color: theme.white
+  }, {
+    backgroundColor: theme.background,
+    color: theme.black
+  }]
+
+  const gridStylesInternal = gridStyles || defaultGridStyles
+
   return (
     <Box
       sx={{
-        minHeight: "100vh",
-        backgroundColor: "background.light",
+        minHeight: '100vh',
+        backgroundColor: 'background',
       }}
     >
       <Grid container columns={columns}>
-        <Grid
-          xs={grid[0]}
-          sx={{
-            minHeight: "100vh",
-            ...(gridStyles[0] ?? {}),
-          }}
-        >
+        <Grid xs={grid[0]} bgcolor={gridStylesInternal[0].backgroundColor} color={gridStylesInternal[0].color} sx={{
+          minHeight: '100vh',
+          overflow: 'hidden'
+        }}>
           {children[0] || null}
         </Grid>
-        <Grid
-          xs={grid[1]}
-          sx={{
-            minHeight: "100vh",
-            ...(gridStyles[1] ?? {}),
-          }}
-        >
+        <Grid xs={grid[1]} bgcolor={gridStylesInternal[1].backgroundColor} color={gridStylesInternal[1].color} sx={{
+          minHeight: '100vh',
+          overflow: 'hidden'
+        }}>
           {children[1] || null}
         </Grid>
       </Grid>
