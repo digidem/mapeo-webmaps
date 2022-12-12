b2106f94af0b8e86b9b019550895c1821d4307e2
lightlii <32508751+lightlii@users.noreply.github.com>
2022-12-12 16:19:11 +0100

    chore: improve typing for ReachRouter-specific components

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from "@mui/material/CssBaseline";
 import { useAuthState } from "react-firebase-hooks/auth";
 import firebase from "firebase/app";
-import { Router, Link, navigate } from "@reach/router"
+import { Router, Link, navigate, RouteComponentProps } from "@reach/router"
 import { IntlProvider } from 'react-intl';
 
 import Login from '../../views/Login'
@@ -11,35 +11,33 @@ import ForgottenPassword from '../../views/ForgottenPassword';
 import theme from '../../theme';
 import { useEffect } from 'react';
 import HomeView from '../../views/Home';
-import { AuthorizedProps } from './types';
 import Signup from '../../views/Signup';
 
 const translations = {
   es: require("../../translations/es.json")
 };
 
-const Authorized = ({ location }: AuthorizedProps) => {
-  const [user, initializing] = useAuthState(firebase.auth());
+const Authorized = ({ location }: RouteComponentProps) => {
+  const [user, initializing] = useAuthState(firebase.auth())
   // Is the user authorized to see this page? An unauthorised user can see any
   // path in the publicPaths map
-  const isAuthorized = user || location?.pathname.startsWith("/auth");
+  const isAuthorized = user || location?.pathname.startsWith("/auth")
   useEffect(() => {
-    if (isAuthorized || initializing) return;
+    if (isAuthorized || initializing) return
     // Redirect unauthorized users to the login page, but keep state of where
     // they come from
     navigate("/auth/login", {
       replace: true,
       state: { from: location?.pathname }
-    });
-  }, [isAuthorized, initializing, location]);
+    })
+  }, [isAuthorized, initializing, location])
 
   return (
     <HomeView />
   );
 };
 
-const lang = navigator.language ? navigator.language.split("-")[0] : "en";
-
+const lang = navigator.language ? navigator.language.split("-")[0] : "en"
 
 const App = () => (
   <IntlProvider locale={lang} messages={translations} defaultLocale="en">
