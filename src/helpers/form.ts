ad142c283e5a46fe7b5fcd98e27daa8f670a53c6
lightlii <32508751+lightlii@users.noreply.github.com>
2022-12-13 15:18:58 +0100

    chore: add reusable form validation functions

--- src/helpers/form.ts
+++ src/helpers/form.ts
@@ -1,22 +1,32 @@
 import { emailRegex } from "./regex"
 
-type errorCodeType = "auth/email-already-in-use" | "auth/invalid-email" | "auth/weak-password"
-export type errorType = {
-  code: errorCodeType
-} | null
+export type SignupErrorCodeType = "auth/email-already-in-use" | "auth/invalid-email" | "auth/weak-password"
+export type SigninErrorCodeType = "auth/invalid-email" | "auth/user-disabled" | "auth/user-not-found" | "auth/wrong-password"
+type errorCodeType = SignupErrorCodeType | SigninErrorCodeType
 
-export const validateEmail = (email: string, callback: (arg0: errorType) => void) => {
+export type SignupErrorType = {
+  code: SignupErrorCodeType
+}
+
+export type SigninErrorType = {
+  code: SigninErrorCodeType
+}
+
+type errorType = SignupErrorType | SigninErrorType | null
+
+export const validateEmail = (email: string, callback: (arg0: any) => void, code: errorCodeType = "auth/invalid-email") => {
   const validEmail = email.match(emailRegex)
 
   if (typeof callback === 'function') {
 
     callback(!validEmail ? {
-      code: "auth/invalid-email"
+      code
     } : null)
   }
 }
 
-export const validatePassword = (password: string, callback: (arg0: errorType) => void, code: errorCodeType = "auth/weak-password") => {
+// TODO: remove 'any' type here ...
+export const validatePassword = (password: string, callback: (arg0?: any) => void, code: errorCodeType = "auth/weak-password") => {
   const validPassword = password.length >= 6
 
   if (typeof callback === 'function') {
