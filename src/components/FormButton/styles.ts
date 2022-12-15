9ffd12a5326c53210404ea56160cfde7af4f9c07
lightlii <32508751+lightlii@users.noreply.github.com>
2022-12-15 11:59:45 +0100

    chore: rename FormButton to Button (since it will be used outside forms)

--- src/components/FormButton/styles.ts
+++
@@ -1,9 +0,0 @@
-import { styled, Button } from "@mui/material";
-
-export const LoadingButton = styled(Button)`
-  &.Mui-disabled {
-    background-color: ${({ theme }) => theme.primary};
-    color: ${({ theme }) => theme.white};
-    opacity: 0.7
-  }
-`;
