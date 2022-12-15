b0dd4eb125e365d67d5b1e45e32de1bf2684106e
lightlii <32508751+lightlii@users.noreply.github.com>
2022-12-15 12:02:07 +0100

    fix: convert colour names in theme to CONSTANT_CASE

--- src/theme.ts
+++ src/theme.ts
@@ -1,20 +1,20 @@
 import { createTheme } from "@mui/system";
 
-const MAPEO_BLUE = "#0066FF";
-const MAPEO_BLACK = "#333333";
-const MAPEO_ORANGE = "#EA7913";
-const DARK_BLUE = "#000033";
-const WARNING_RED = "#D92222";
+const MAPEO_BLUE = '#0066FF'
+const MAPEO_BLACK = '#333333'
+const MAPEO_ORANGE = '#EA7913'
+const DARK_BLUE = '#000033'
+const WARNING_RED = '#D92222'
 
 export const theme = createTheme({
   typography: {
     fontFamily: [
-      "Rubik",
-      "Roboto",
+      'Rubik',
+      'Roboto',
       '"Segoe UI"',
       '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
+      'Arial',
+      'sans-serif',
       '"Apple Color Emoji"',
       '"Segoe UI Emoji"',
       '"Segoe UI Symbol"',
@@ -22,16 +22,16 @@ export const theme = createTheme({
   },
   palette: {
     background: {
-      light: "#F4F4FF",
+      light: '#F4F4FF',
       dark: DARK_BLUE,
     },
     primary: MAPEO_BLUE,
     error: {
-      main: WARNING_RED,
+      main: WARNING_RED
     },
     black: MAPEO_BLACK,
-    orange: MAPEO_ORANGE,
-  },
+    orange: MAPEO_ORANGE
+  }
 });
 
 export default theme
