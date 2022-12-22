f35a3ce8fd35056d7669cb873ec24ac1dcde2f37
lightlii <32508751+lightlii@users.noreply.github.com>
2022-12-22 16:58:58 +0100

    feat: add AuthoriseLayout

---
+++ src/layouts/Authorised/index.tsx
@@ -0,0 +1,31 @@
+import { Box, LinearProgress, Stack } from "@mui/material";
+import { maxWidth } from "@mui/system";
+import Header from "../../components/Header";
+
+type AuthorisedLayoutProps = {
+  renderHeader?: React.ComponentType
+  children: React.ReactNode
+  loading?: boolean
+}
+
+export const AuthorisedLayout = ({ renderHeader: CustomHeader, children, loading }: AuthorisedLayoutProps) => {
+  return (
+    <Box
+      sx={{
+        minHeight: '100vh',
+        backgroundColor: 'background',
+      }}
+    >
+      <>
+        <Header>{CustomHeader ? <CustomHeader /> : null}</Header>
+        {loading ? <Loader /> : children}
+      </>
+    </Box>
+  )
+}
+
+const Loader = () => (
+  <Stack justifyContent="center" alignItems="center" sx={{ height: 'calc(100vh - 80px)' }}>
+    <LinearProgress color="primary" sx={{ height: '10px', maxWidth: '70vw' }} />
+  </Stack>
+)
