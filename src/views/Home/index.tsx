97ac9e012a062d29f5cdfd5557cb7c71bd5e6555
lightlii <32508751+lightlii@users.noreply.github.com>
2023-01-12 15:03:36 +0100

    chore: basic dropzone implementation for file upload

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -2,108 +2,55 @@ import { Fade, useTheme, Box, Link, Typography } from '@mui/material'
 import { Stack } from '@mui/system'
 
 import { useIntl } from 'react-intl'
-import { useDropzone } from 'react-dropzone'
-import { useCallback } from 'react'
-
-import { Overlay } from './styles'
+import { AddMapButton } from '../../components/AddMapButton'
+import { AuthorisedLayout } from '../../layouts/Authorised'
+import { Img } from './styles'
+import { useEffect } from "react";
+import { DropzoneInputProps, DropzoneRootProps, useDropzone } from "react-dropzone";
 import { messages as msgs } from './messages'
 
-import { Loader } from '../../components/Loader'
-import { Button } from '../../components/Button'
-import { MapsList } from '../../components/MapsList'
-import { AuthorisedLayout } from '../../layouts/Authorised'
-import { ProgressType, useCreateMap } from '../../hooks/useCreateMap'
-import { Container } from '../../components/Container'
-
 export const HomeView = () => {
-  const theme = useTheme()
-
-  const {
-    createMap,
-    progress: { loading: uploading, failedFiles },
-    progress,
-  } = useCreateMap()
-
-  const onDrop = useCallback(
-    (acceptedFiles: File[]) => {
-      if (!acceptedFiles.length || !acceptedFiles[0].name.match(/.mapeomap$/)) return
-      createMap(acceptedFiles)
-    },
-    [createMap],
-  )
-
-  const { getRootProps, getInputProps, open, isDragActive } = useDropzone({
+  const { getRootProps, getInputProps, open, acceptedFiles, isDragActive } = useDropzone({
     noClick: true,
-    noKeyboard: true,
-    accept: ['.mapeomap'],
-    onDrop,
-  })
+    noKeyboard: true
+  });
+
+  useEffect(() => {
+    console.log({ acceptedFiles })
+  }, [acceptedFiles])
+
 
   return (
     <AuthorisedLayout onClickAddMap={open}>
-      <Stack
-        sx={{
-          overflowY: 'scroll',
-          '&::-webkit-scrollbar': {
-            width: '10px' /* width of the entire scrollbar */,
-          },
-          '&::-webkit-scrollbar-track': {
-            background: theme.background /* color of the tracking area */,
-          },
-          '&::-webkit-scrollbar-thumb': {
-            backgroundColor: theme.blueDark /* color of the scroll thumb */,
-            borderRadius: 0 /* roundness of the scroll thumb */,
-          },
-        }}
-      >
-        <input {...getInputProps()} />
-        {uploading || failedFiles?.length ? (
-          <Uploading progress={progress} />
-        ) : (
-          <div {...getRootProps({ className: 'dropzone' })}>
-            <DragDropOverlay active={isDragActive} />
-            <MapsList openDialog={open} isDragActive={isDragActive} />
-          </div>
-        )}
-      </Stack>
-    </AuthorisedLayout>
-  )
+      <NoMaps openDialog={open} getRootProps={getRootProps} getInputProps={getInputProps} isDragActive={isDragActive} />
+    </AuthorisedLayout>)
 }
 
-const DragDropOverlay = ({ active }: { active: boolean }) => {
+type NoMapsType = {
+  openDialog: () => void
+  getRootProps: (props?: DropzoneRootProps | undefined) => DropzoneRootProps
+  getInputProps: (props?: DropzoneInputProps | undefined) => DropzoneInputProps
+  isDragActive: boolean
+}
+
+const NoMaps = ({ openDialog, getRootProps, getInputProps, isDragActive }: NoMapsType) => {
   const { formatMessage } = useIntl()
 
   return (
-    <Fade in={active}>
-      <Overlay>
-        <span>
-          <Typography variant="h2" align="center" color="white">
-            {formatMessage(msgs.drop_file_message)}
+    <div {...getRootProps({ className: 'dropzone' })}>
+      <Box justifyContent="center" alignItems="center" sx={{ width: '100%', height: 'calc(100vh - 80px)', paddingTop: '15vh' }}>
+        <Stack direction="column" justifyContent="center" alignItems="center" spacing={5}>
+          <Img src="/svg/nomap.svg" alt="" />
+          <Typography variant="h3">No maps to show</Typography>
+          <Typography variant="body1">
+            {formatMessage(msgs.empty_message)}
+            <Link href={formatMessage(msgs.empty_message_href)}>{formatMessage(msgs.empty_message_link)}</Link>
           </Typography>
-        </span>
-      </Overlay>
-    </Fade>
-  )
-}
-
-type UploadingType = {
-  progress: ProgressType
-}
-
-const Uploading = ({
-  progress: { completed, loading, error, currentFile, failedFiles, retryFailedFiles },
-}: UploadingType) =>
-  failedFiles?.length && !loading ? (
-    <>
-      <Button onClick={retryFailedFiles}>Retry</Button>
-      {failedFiles.length ? (
-        <Typography variant="body1">
-          {formatMessage(msgs.empty_message)}
-          <Link href={formatMessage(msgs.empty_message_href)}>{formatMessage(msgs.empty_message_link)}</Link>
-        </Typography>
-        <AddMapButton />
-      </Stack>
-    </Box >
+          <input {...getInputProps()} />
+          <AddMapButton onClick={openDialog} />
+        </Stack>
+      </Box >
+    </div>
   )
 }
 
