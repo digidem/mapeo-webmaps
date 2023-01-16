7b9cd6d9ea4dd01ae6bc411675fdc753a26d1d14
lightlii <32508751+lightlii@users.noreply.github.com>
2023-01-16 11:42:28 +0100

    chore: remove redundent types and  use package types from ReachRouter

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,7 @@
 import { WindowLocation } from '@reach/router'
-import { FieldValue } from 'firebase/firestore'
 
 export type LocationProps = WindowLocation & {
   state: {
-    from?: string
-  }
-}
-
-export type MapMetadataType = {
-  createdAt?: FieldValue
-  description?: string
-  public?: boolean
-  title: string
-  mapStyle?: string
-  terms?: string
-}
+    from?: string;
+  };
+};
