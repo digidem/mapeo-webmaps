1b58c9dcff452f2eea42c8b75af4e12eea296501
lightlii <32508751+lightlii@users.noreply.github.com>
2023-01-23 17:04:42 +0100

    chore: Rename login -> Signup for consistency

--- src/views/Signin/index.tsx
+++ src/views/Signin/index.tsx
@@ -1,7 +1,6 @@
 import { RouteComponentProps } from '@reach/router'
 import { SignInForm } from './SignInForm'
 import { AuthPanel } from '../../components/AuthPanel'
-// import { RouteType } from "../../types"
 
 export const LoginView = ({}: RouteComponentProps) => (
   <AuthPanel>
