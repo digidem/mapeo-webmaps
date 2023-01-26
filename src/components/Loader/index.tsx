56ebe529738ab895f96895164f8977dc55cfed7f
lightlii <32508751+lightlii@users.noreply.github.com>
2023-01-26 16:08:33 +0100

    feat: add dropzone overlay when dropping file

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,7 +1,9 @@
 import { LinearProgress, Stack } from '@mui/material'
 
-export const Loader = () => (
-  <Stack justifyContent="center" alignItems="center" sx={{ height: 'calc(100vh - 80px)' }}>
-    <LinearProgress color="primary" sx={{ height: '10px', maxWidth: '70vw' }} />
+type LoaderTypes = { width?: number; justify?: 'flex-start' | 'flex-end' | 'center' }
+
+export const Loader = ({ width = 70, justify = 'center' }: LoaderTypes) => (
+  <Stack justifyContent={justify} alignItems="center" sx={{ height: 'calc(100vh - 80px)' }}>
+    <LinearProgress color="primary" sx={{ height: '10px', width: `${width}%` }} />
   </Stack>
 )
