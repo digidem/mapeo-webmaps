d12b870ed09e060f36567f21dad68cff3b6b69b8
lightlii <32508751+lightlii@users.noreply.github.com>
2023-01-30 16:37:35 +0100

    fix: merge from stash after another(..) messy rebase

--- src/views/Home/styles.ts
+++ src/views/Home/styles.ts
@@ -4,4 +4,19 @@ import { styled } from '@mui/system'
 export const Img = styled('img')`
   width: 350px;
   height: 350px;
-`;
+`
+
+export const Overlay = styled('div')`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  z-index: 100;
+  background-color: #000000cc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  span {
+    max-width: 800px;
+  }
+`
