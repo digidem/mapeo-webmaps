1ceb5cd30490ab97d2c0bf9456be266d39c17bb6
lightlii <32508751+lightlii@users.noreply.github.com>
2023-01-30 16:48:00 +0100

    fix: more rebase fixes

--- src/views/Home/styles.ts
+++ src/views/Home/styles.ts
@@ -10,7 +10,7 @@ export const Overlay = styled('div')`
   width: 100vw;
   height: 100vh;
   position: fixed;
-  z-index: 100;
+  z-index: 1000;
   background-color: #000000cc;
   display: flex;
   justify-content: center;
