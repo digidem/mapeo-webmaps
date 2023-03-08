0cc4c1e205d4bcb2962435181ee53956e6619653
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-03-08 15:15:45 -0800

    simplified use timeout

--- src/hooks/utility.ts
+++ src/hooks/utility.ts
@@ -2,28 +2,6 @@ import { useEffect, useRef, useState } from 'react'
 
 type UseTimeoutReturnType = [boolean, () => void]
 
-export const useTimeout = (callback: () => void, delay: number | null) => {
-  const timeoutRef = useRef<number | null>(null)
-  const savedCallback = useRef(callback)
-
-  useEffect(() => {
-    savedCallback.current = callback
-  }, [callback])
-
-  useEffect(() => {
-    const tick = () => savedCallback.current()
-    if (delay !== null) {
-      timeoutRef.current = window.setTimeout(tick, delay)
-      return () => {
-        if (timeoutRef.current) {
-          window.clearTimeout(timeoutRef.current)
-        }
-      }
-    }
-  }, [delay])
-  return timeoutRef
-}
-
 export const useTimeoutState = (delay: number): UseTimeoutReturnType => {
   const [isActive, setIsActive] = useState(false)
   const timeout = useRef<number | null>(null)
